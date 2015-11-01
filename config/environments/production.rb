6149cf8704373273bc3df35fd11cd148f6dc2ef9
David Bernheisel <david@bernheisel.com>
2015-11-01 17:51:02 -0500

    Trying deployment again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
