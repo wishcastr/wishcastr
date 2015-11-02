e2c6ba188b14807d85800e99b46cd946f70628a2
David Bernheisel <david@bernheisel.com>
2015-11-01 20:32:18 -0500

    Trying deployment again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.serve_static_files = true
   config.assets.compile = true
 
   # Eager load code on boot. This eager loads most of Rails and
@@ -19,7 +18,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  config.serve_static_files = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
