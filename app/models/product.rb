e51a641d7602f56c2ffd1fa0185be7bd06114945
David Bernheisel <david@bernheisel.com>
2015-11-02 17:09:39 -0500

    Add scope

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
   has_many :products_wish
   has_many :wishes, through: :products_wish
+
+  scope :by_wishes, -> { Product.all.sort { |a,b| a.wishes.count <=> b.wishes.count}.reverse }
 end
