7b23ab9e72d0b19b4e714fbdf4e06b64cae40542
David Bernheisel <david@bernheisel.com>
2015-11-02 18:59:54 -0500

    Update CORS for localhost testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost*'
+    origins 'localhost:3010'
 
     resource '*',
       headers: :any,
