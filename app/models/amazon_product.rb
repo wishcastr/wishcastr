be25d5d7a0f0e34f6fca61251c320e50f13d737b
Da-Me <damekim1206@gmail.com>
2015-11-03 18:28:25 -0500

    add method in product model

--- app/models/amazon_product.rb
+++ app/models/amazon_product.rb
@@ -5,45 +5,48 @@ require 'base64'
 
 class AmazonProduct < Product
 
-  key = ENV['AMAZON_ACCESS_KEY_ID']
-  secret = ENV['AMAZON_SECRET_ACCESS_KEY']
+  def search(query)
+    key = ENV['AMAZON_ACCESS_KEY_ID']
+    secret = ENV['AMAZON_SECRET_ACCESS_KEY']
 
-  AWS_ACCESS_KEY_ID = key
-  AWS_SECRET_KEY = secret
+    # The region you are interested in
+    ENDPOINT = "webservices.amazon.com"
+    REQUEST_URI = "/onca/xml"
 
-  # The region you are interested in
-  ENDPOINT = "webservices.amazon.com"
+    params = {
+      "Service" => "AWSECommerceService",
+      "Operation" => "ItemSearch",
+      "AWSAccessKeyId" => key,
+      "AssociateTag" => "wishcaster-20",
+      "SearchIndex" => "All",
+      "Keywords" => query,
+      "ResponseGroup" => "Images,ItemAttributes,ItemIds,OfferListings,Offers"
+    }
 
-  REQUEST_URI = "/onca/xml"
+    # Set current timestamp if not set
+    params["Timestamp"] = Time.now.gmtime.iso8601 if !params.key?("Timestamp")
 
-  params = {
-    "Service" => "AWSECommerceService",
-    "Operation" => "ItemSearch",
-    "AWSAccessKeyId" => key,
-    "AssociateTag" => "wishcaster-20",
-    "SearchIndex" => "All",
-    "Keywords" => "#{:search}",
-    "ResponseGroup" => "Images,ItemAttributes,ItemIds,OfferListings,Offers"
-  }
+    # Generate the canonical query
+    canonical_query_string = params.sort.collect do |key, value|
+      [URI.escape(key.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")), URI.escape(value.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))].join('=')
+    end.join('&')
 
-  # Set current timestamp if not set
-  params["Timestamp"] = Time.now.gmtime.iso8601 if !params.key?("Timestamp")
+    # Generate the string to be signed
+    string_to_sign = "GET\n#{ENDPOINT}\n#{REQUEST_URI}\n#{canonical_query_string}"
 
-  # Generate the canonical query
-  canonical_query_string = params.sort.collect do |key, value|
-    [URI.escape(key.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")), URI.escape(value.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))].join('=')
-  end.join('&')
+    # Generate the signature required by the Product Advertising API
+    signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), AWS_SECRET_KEY, string_to_sign)).strip()
 
-  # Generate the string to be signed
-  string_to_sign = "GET\n#{ENDPOINT}\n#{REQUEST_URI}\n#{canonical_query_string}"
+    # Generate the signed URL
+    request_url = "http://#{ENDPOINT}#{REQUEST_URI}?#{canonical_query_string}&Signature=#{URI.escape(signature, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}"
 
-  # Generate the signature required by the Product Advertising API
-  signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), AWS_SECRET_KEY, string_to_sign)).strip()
+    logger.debug("Signed URL: \"#{request_url}\"")
+    return "Signed URL: \"#{request_url}\""
+  end
 
-  # Generate the signed URL
-  request_url = "http://#{ENDPOINT}#{REQUEST_URI}?#{canonical_query_string}&Signature=#{URI.escape(signature, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}"
-
-  puts "Signed URL: \"#{request_url}\""
+  def
+    
+  end
 
 
 end
