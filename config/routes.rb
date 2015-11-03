f599fdb61324fe0823405a096c5d6e8087151ed7
David Bernheisel <david@bernheisel.com>
2015-11-03 09:31:45 -0500

    Update gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       get 'top'
     end
   end
+  mount ApiDocs::Engine => '/api-docs'
 
   resources :products_wishes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
