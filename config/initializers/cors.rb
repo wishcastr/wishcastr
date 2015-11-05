4aa1b8a9063c3af77e99aa2debe45d3f70b39d07
David Bernheisel <david@bernheisel.com>
2015-11-05 11:31:50 -0500

    Rebuild

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /localhost(:\d+)/
+    origins /localhost(:\d+)/, 'wishcastr-staging.herokuapp.com'
 
     resource '*',
       headers: :any,
