4733ca1aa3c827ec2219c539608d4d56635beab5
Da-Me <damekim1206@gmail.com>
2015-11-06 16:12:27 -0500

    add format to pass update products in products controller test

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -22,17 +22,22 @@ class ProductsControllerTest < ActionController::TestCase
     end
     assert_response 201
   end
-  #
-  # test "should show product" do
-  #   get :show, params: { id: @product }
-  #   assert_response :success
-  # end
-  #
-  # test "should update product" do
-  #   patch :update, params: { id: @product, product: { affiliate_url: @product.affiliate_url, brand: @product.brand, description: @product.description, image_large: @product.image_large, image_thumbnail: @product.image_thumbnail, sku: @product.sku, title: @product.title, type: @product.type } }
-  #   assert_response 200
-  # end
-  #
+
+  test "should show product" do
+    get :show, params: { id: @product }
+    assert_response :success
+  end
+
+  test "should update product" do
+    patch :update, params: { id: @product,
+      product: { affiliate_url: @product.affiliate_url,
+      brand: @product.brand, description: @product.description,
+      image_large: @product.image_large,
+      image_thumbnail: @product.image_thumbnail, sku: @product.sku,
+      title: @product.title, type: @product.type } }, format: :json
+    assert_response 200
+  end
+
   # test "should destroy product" do
   #   assert_difference('Product.count', -1) do
   #     delete :destroy, params: { id: @product }
