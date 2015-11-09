b875bc491d4a4fdf1fbdbcc87d84d7960a5d18a3
Da-Me <damekim1206@gmail.com>
2015-11-09 14:48:33 -0500

    generate mailer

---
+++ app/mailers/catch_mailer.rb
@@ -0,0 +1,13 @@
+class CatchMailer < ApplicationMailer
+
+  # def wishes(wishes, email)
+  #   @mail_time = Time.now
+  #   @wish = Wish.find_by(wish)
+  #   @products = Product.select("products.*, COUNT(products_wishes.id) as wishes_count")
+  #     .joins("LEFT OUTER JOIN products_wishes ON products.id = products_wishes.product_id")
+  #     .group("products.id")
+  #     .order("wishes_count DESC")
+  #   how_many ? products.limit(how_many) : products
+
+  # end
+end
