faf8ab4e20674d924b9254d890ba0a35b21489a4
David Bernheisel <david@bernheisel.com>
2015-11-09 01:18:56 -0500

    Add favicon

--- config/initializers/cors.rb
+++
@@ -1,6 +0,0 @@
-# Avoid CORS issues when API is called from the frontend app
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests
-
-# Read more: https://github.com/cyu/rack-cors
-
-# see config.ru
