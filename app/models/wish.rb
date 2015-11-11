db182a7cadcdc4ee2297c2cc67b7507c53e928ef
Da-Me <damekim1206@gmail.com>
2015-11-11 12:24:03 -0500

    add some mo' stars to Da-Me about page

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -4,6 +4,15 @@ class Wish < ActiveRecord::Base
   belongs_to :user
 
   def compare
+    # @product = wish.products
+    # self.each do |w|
+  end
+
+  def check_products
+    product = self.products
+    self.each do |w|
+      w.product
+
     loop through wishes, loop through products attached to that wish, then compare
     prices of that of the same sku and if any of the threshold prices are met then
     trigger mailer. also replace old prices_history with most current prices_history
