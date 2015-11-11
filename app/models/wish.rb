efeac7d4d092b591b999a97755ea905dc2a4710b
Da-Me <damekim1206@gmail.com>
2015-11-11 11:50:33 -0500

    add walkthrough of what we want in method compare in wish model

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -3,4 +3,11 @@ class Wish < ActiveRecord::Base
   has_many :products, through: :products_wish
   belongs_to :user
 
+  def compare
+    loop through wishes, loop through products attached to that wish, then compare
+    prices of that of the same sku and if any of the threshold prices are met then
+    trigger mailer. also replace old prices_history with most current prices_history
+
+  end
+
 end
