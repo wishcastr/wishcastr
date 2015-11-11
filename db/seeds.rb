5c668554f3d317b3375acbbaa47d2d9dc50e4809
David Bernheisel <david@bernheisel.com>
2015-11-11 08:43:13 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-700.times do
+100.times do
   User.create!(
     name: Faker::Name.name,
     email: Faker::Internet.email,
