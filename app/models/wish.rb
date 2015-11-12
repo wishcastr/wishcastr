856e13752f4f9ce43c21f010a2375f41632bebf9
David Bernheisel <david@bernheisel.com>
2015-11-11 20:18:30 -0500

    Remove dead code

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -3,15 +3,6 @@ class Wish < ActiveRecord::Base
   has_many :products, through: :products_wish
   belongs_to :user
 
-  def check_products
-    products
-    # self.each do |w|
-    #   w.product
-
-    # loop through wishes, loop through products attached to that wish, then compare
-    # prices of that of the same sku and if any of the threshold prices are met then
-    # trigger mailer. also replace old prices_history with most current prices_history
-
   def find_catches
     catches = []
     self.products.each do |p|
