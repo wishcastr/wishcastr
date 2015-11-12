bf066796403d55d892b0910a1ae5f952ff08d0f4
Da-Me <damekim1206@gmail.com>
2015-11-12 15:55:36 -0500

    add new account creation email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ActiveRecord::Base
   validates :amz_id, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
   has_many :wishes
+  after_create :new_account_email
 
+
+  def new_account_email
+    CatchMailer.new_account(self).deliver_now
+  end
 end
