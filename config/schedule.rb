c67388a11ac5d33a112654de45e8dc842a6c20d0
Da-Me <damekim1206@gmail.com>
2015-11-14 15:19:34 -0500

    update scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,13 +18,14 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
-# every 10.minutes do
-#   Product.update_prices
-#   Wishes.all.each do |wish|
-#     catches = wish.find_catches
-#     catches.each do |caught|
-#       CatchMailer.catch(wish.user, caught, wish).deliver_now unless wish.notified
-#       wish.update(notified: true)
-#     end
-#   end
-# end
+group: do 'production'
+every 30.minutes do
+  Product.update_prices
+  Wishes.all.each do |wish|
+    catches = wish.find_catches
+    catches.each do |caught|
+      CatchMailer.catch(wish.user, caught, wish).deliver_now unless wish.notified
+      wish.update(notified: true)
+    end
+  end
+end
