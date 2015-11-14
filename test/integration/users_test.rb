422eab740e5e0b350493d70cf1ded36daf050801
Da-Me <damekim1206@gmail.com>
2015-11-14 15:25:53 -0500

    add integration test

--- test/integration/users_test.rb
+++ test/integration/users_test.rb
@@ -1,5 +1,11 @@
 require 'test_helper'
 
 class UsersTest < ActionDispatch::IntegrationTest
-
+  # 
+  # test 'user can login' do
+  #   get users_path
+  #   assert_redirected_to show_new_path
+  #   follow_redirect!
+  #   assert_template 'show'
+  # end
 end
