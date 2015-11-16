a71e2d1306c79a4caad0d30913324008a3caa6d8
Da-Me <damekim1206@gmail.com>
2015-11-16 13:28:09 -0500

    add more integration test

--- test/integration/wishes_test.rb
+++ test/integration/wishes_test.rb
@@ -3,18 +3,29 @@ require 'test_helper'
 class WishesTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
-    @wishes = wishes(:one)
+    @wish = wishes(:one)
   end
 
-    test 'show user wish' do
-      get "/wishes/1.json"
-      assert_template "[]"
-    end
+  test 'show user wish' do
+    get "/wishes/" + @wish.id.to_s, params: {
+      user_id: @user.id,
+      access_token: @user.amz_access_token
+    }
+    assert_template :show
+  end
 
-    test 'user can update wish' do
-      patch "/wishes/1.json", params: {user_id: 1, threshold_price: 24,
-         category: "Game", query: "Halo", name: "Halo:Reach"}
-      assert_template :show
-    end
+  test 'user can update wish' do
+    patch "/wishes/" + @user.wishes.first.id.to_s, params: {
+      wish: {
+        threshold_price: 24,
+        category: "Game",
+        query: "Halo",
+        name: "Halo:Reach"
+      },
+      user_id: @user.id,
+      access_token: @user.amz_access_token
+    }
+    assert_template :show
+  end
 
 end
