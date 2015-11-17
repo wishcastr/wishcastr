f3ce84d507b8de4d6d3b17c4a54c7671b6673e2b
Da-Me <damekim1206@gmail.com>
2015-11-17 11:22:57 -0500

    update yml cc

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,12 +5,12 @@ class UserTest < ActiveSupport::TestCase
     @user = users(:one)
     @wish = wishes(:one)
   end
-  test "draft wish method" do
-    wishes = []
-    u = User.create(id: 1, name: "MyString", email:"MyString@you.com")
-    w = Wish.create(user_id: 1)
-    wishes << w
-    assert true
-  end
+  # test "draft wish method" do
+  #   wishes = []
+  #   u = User.create(id: 1, name: "MyString", email:"MyString@you.com")
+  #   w = Wish.create(user_id: 1)
+  #   wishes << w
+  #   assert true
+  # end
 
 end
