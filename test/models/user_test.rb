460ae22c25425be420289f43012f984ed0e49c29
Da-Me <damekim1206@gmail.com>
2015-11-17 11:20:11 -0500

    update test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    @wish = wishes(:one)
+  end
+  test "draft wish method" do
+    wishes = []
+    u = User.create(id: 1, name: "MyString", email:"MyString@you.com")
+    w = Wish.create(user_id: 1)
+    wishes << w
+    assert true
+  end
+
 end
