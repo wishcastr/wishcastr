7baecb7e2cc0c7faf832d2d7f41c6b925dcc61c6
Da-Me <damekim1206@gmail.com>
2015-11-16 22:06:46 -0500

    update wish model

--- test/models/wish_test.rb
+++ test/models/wish_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class WishTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "product duplicate method" do
+    wish = Wish.create(id: 1, user_id: 1, category: "toys")
+    product = Product.create!(id:1, sku: "123qwe", title: "Star Wars")
+    wish.product_duplicate?("123qwe", Wish)
+
+    assert true
+  end
+
 end
