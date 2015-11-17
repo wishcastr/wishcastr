dd11fa03684b60fbb740ad94a0ecbb38c23dd03e
Da-Me <damekim1206@gmail.com>
2015-11-17 09:47:44 -0500

    update mailer test

--- test/mailers/catch_mailer_test.rb
+++ test/mailers/catch_mailer_test.rb
@@ -15,13 +15,8 @@ class CatchMailerTest < ActionMailer::TestCase
     assert_match "Caught", email.body.encoded
   end
 
-  # test "account created" do
-  #     email = CatchMailer.new_account()
-  #     assert_not ActionMailer::Base.deliveries.empty?
-  #
-  #     assert_equal ['catch@wishcastr.com'], email.from
-  #     assert_equal ['david@wishcastr.com'], email.to
-  #     assert_equal 'Thanks for joining us!', email.subject
-  #     assert_equal read_fixture('Welcome').join, email.body.to_s
-  # end
+  test "could not find user" do
+    email = CatchMailer.catch("David")
+    assert "Could not find David"
+  end
 end
