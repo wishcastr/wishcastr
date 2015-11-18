be52911028e35b7e42fbf3932a3c1b964fea6343
David Bernheisel <david@bernheisel.com>
2015-11-18 17:35:05 -0500

    rebuild

--- src/js/main.js
+++ src/js/main.js
@@ -365,7 +365,7 @@
   })// END TABS CONTROLLER
 
   .constant('API', {
-    BASE_URL: '//wishcastr-staging.herokuapp.com',
+    BASE_URL: '//',
     SEARCH_PATH: '/products/search.json',
     DRAFT_WISH: 'draft.json',
     WISHES_PATH: '/wishes.json',
